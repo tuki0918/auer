de3272b31f6befe5110935597aeb3c38ca4398ec
tuki0918 <tuki0918@gmail.com>
2016-04-11 22:13:14 +0900

    テストスイートの修正

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -1,12 +1,44 @@
 <?php
 
+use Auer\Log\Logger;
+
 class LoggerTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @test
+     * @expectedException RuntimeException
      */
-    public function テストスイート()
+    public function 静的メソッドの確認_セットアップ前()
     {
-        self::assertEquals(1, 1);
+        Logger::push();
+    }
+
+    /**
+     * @test
+     * @depends 静的メソッドの確認_セットアップ前
+     */
+    public function セットアップ()
+    {
+        $stream = __DIR__ . '/../logs/test.log';
+        return Logger::init($stream);
+    }
+
+    /**
+     * @test
+     * @depends セットアップ
+     * @param Logger $logger
+     */
+    public function セットアップ後の型確認($logger)
+    {
+        static::assertInstanceOf(Monolog\Logger::class, $logger);
+    }
+
+    /**
+     * @test
+     * @depends セットアップ後の型確認
+     */
+    public function 静的メソッドの確認_セットアップ後()
+    {
+        static::assertInstanceOf(Monolog\Logger::class, Logger::push());
     }
 }
